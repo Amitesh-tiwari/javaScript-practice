20dfa137e0d02cda5e1a2dc27f896faa0a4554b5
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-16 18:40:08 +0530

    String Methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -9,9 +9,12 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
+//Creates the String by calling the String constructor and generates the object
+// and mapping of characters with the indexes.
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+ console.log(gameName[0]);
+ //A way of accessing the prototype of the String and returns the object in return.
+ console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
