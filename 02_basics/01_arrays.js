e66870cd701ebf80ca8ebb8904e132a06e63165a
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-17 17:44:05 +0530

    arrays in JS

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -18,10 +18,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+ const newArr = myArr.join()
+ //make the type of array to string
 
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr);
+console.log( newArr);
+//gives the same value but in the form of stirng
 
 
 // slice, splice
@@ -29,6 +31,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
+//slice does not include the last range in the array and 
+//it does not change the original array.
 
 console.log(myn1);
 console.log("B ", myArr);
@@ -37,3 +41,5 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+//splice include the last range in the array and it also does
+//the changes in the original array, it manipulates the original array.
