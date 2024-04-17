74f56d795490426859e445eb9c6d0abf0edf37ba
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-04-17 15:57:28 +0530

    Math in javascript

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,15 +4,19 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+ console.log(otherNumber.toPrecision(4));
+ //rerurns the number with the precision of 4 digits in the form 
+ //of string.
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
+//Locale String is used to represent the zeros with commas 
+//for better representation of the number.
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
@@ -25,8 +29,12 @@ const hundreds = 1000000
 // console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
+//gives the random numners between 0 and 1.
 console.log((Math.random()*10) + 1);
+//we multiply it by 10 to shift the digits to left side to
+//increase the range and 1 is added bcoz value can be 0.04 so +1 will never let it be 0.
 console.log(Math.floor(Math.random()*10) + 1);
+//returns the floor value from the random valude.
 
 const min = 10
 const max = 20
